6ca92b5a2b6259d58c1071eb161f6ee939dbe011
daniel <danlikestech@gmail.com>
2022-01-22 14:37:42 -0600

    fixed errors

--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -4,32 +4,20 @@ const Blog = require("../schemas/blog");
 
 const blogRoute = express.Router();
 
-blogRoute.get('./:username', jwt.authenticateToken, (req, res) => {
-
-})
+blogRoute.get("./:username", jwt.authenticateToken, (req, res) => {});
 
 blogRoute.post("./:username", jwt.authenticateToken, (req, res) => {
-    Let username = req.params.username
-    Let blogPost = req.blogPost = req.body
-    blogPost.created_by = username
-    blogPost.created_at = Date.now()
+  let username = req.params.username;
+  let blogPost = (req.blogPost = req.body);
+  blogPost.created_by = username;
+  blogPost.created_at = Date.now();
 
-    Blog.create(blogPost, (err, blog) => {
-        if(err) {
-            res.status(400).json({ message: err.message})
-        }
-         res.status(400).json({ message: blog})
-        })
-    })
-
-    
+  Blog.create(blogPost, (err, blog) => {
+    if (err) {
+      res.status(400).json({ message: err.message });
+    }
+    res.status(400).json({ message: blog });
+  });
 });
-
-
-
-
-
-
-
 
 blogRoute.delete("/id", jwt.authenticateToken, (req, res) => {});
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,7 +2,6 @@ const express = require("express");
 const bcrypt = require("bcrypt");
 const User = require("../schemas/user");
 const jwt = require("../middleware/jwt");
-let salt = this.process.env.SALT
 
 const userRouter = express.Router();
 
@@ -10,35 +9,43 @@ userRouter.get("/", (req, res) => {
   res.status(200).json({ message: "Users Up" });
 });
 
-userRouter.post("/register", (req, res) => {
-  res.status(200).json({ message: "Users Registered" });
+userRouter.post("/register", async (req, res) => {
+  let user = req.body;
+  let password = req.body.password;
+  let salt = Number(process.env.SALT);
+  let hashedPassword = await bcrypt.hash(password, salt);
+  user.password = hashedPassword;
+
+  User.create(user, (err, newUser) => {
+    if (err) {
+      res.status(404).json({ message: err.message });
+    }
+    res.status(200).json({ user: newUser });
+  });
 });
 
 userRouter.post("/login", async (req, res) => {
-    Let username = req.body.username
-    Let password = req.body.password
-    Let salt = Number()
+  let username = req.body.username;
+  let password = req.body.password;
 
-    User.findOne({ username: username}, (err, retUser) => {
-        if(err) {
-            res.status(404).json({message: err.message})
-        }
-        bcrypt.compare(password, retUser.password, (err, result) => {
-             if(err) {
-            res.status(400).json({message: err.message})
-        }
+  User.findOne({ username: username }, (err, retUser) => {
+    if (err) {
+      res.status(404).json({ message: err.message });
+    }
+    bcrypt.compare(password, retUser.password, (err, result) => {
+      if (err) {
+        res.status(400).json({ message: err.message });
+      }
 
-        if(result === false) {
-            res.status(403).json({message: "You need to be logged in"})
-        }
+      if (result === false) {
+        res.status(403).json({ message: "You need to be logged in" });
+      }
 
-        Let token = jwt.generateAccessToken(retUser.username)
-        res.setHeader("Auth", token)
-        res.status(200).json({message: retUser})
-        })
-
-    })
- 
+      let token = jwt.generateAccessToken(retUser.username);
+      res.setHeader("Auth", token);
+      res.status(200).json({ message: retUser });
+    });
+  });
 });
 
 module.exports = userRouter;
